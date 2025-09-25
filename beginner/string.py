81fdc1b406874c1f9c207201379f7c1e6cbdb522
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-25 04:43:33 +0000

    Changes to be committed: 	modified:   beginner/string.py

--- beginner/string.py
+++ beginner/string.py
@@ -1,23 +1,65 @@
 
 
 print("hello world")
+'''
+string are always wtitten like this
 
-# string are always wtitten like this
-# ''
-# ""
-# ''''''
+'  '
+"  "
+'''  '''
 
+'''
 
 # indexing :: 
-#         use [] braces to acess indeing o the char individually
-'''
+#         use [] braces to acess indeing the char individually
+
 institute = "the kiran academy "
 
 print(institute[0])
 print(institute[-8])
 
+
+# slicing
+#           it capture the character untill u say so
+
+
+name = "sairaj rajendra khilari"
+
+# this indexing tells that  normal character capture
+print(name[0:6:1])
+
+# 
+
+
+
+
+
+
+'''
+
+city = "rameshwaram"
+
+print(city[0:3:1])
+print(city[2:6:1])
+print(city[-5:-2:1])
+print(city[4:1:-1])
+print(city[-1:-4:-1])
 '''
 
 
+institute = "the kiran academy"
 
 
+print(institute[0:3:1])
+print(institute[4:9:1])
+print(institute[-1:-8:-1])
+print(institute[8:3:-1])
+print(institute[-7::1])
+print(institute[2::-1])
+
+
+
+lan = "python programming language"
+
+print(lan[:6:])
+print(lan[:-9:-1])
