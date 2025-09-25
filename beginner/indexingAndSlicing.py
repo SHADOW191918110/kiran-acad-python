cf9edf69cad8658c8160b33849ec5aab7a165c76
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-25 04:51:15 +0000

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	renamed:    beginner/string.py -> beginner/indexingAndSlicing.py

--- beginner/indexingAndSlicing.py
+++ beginner/indexingAndSlicing.py
@@ -46,7 +46,7 @@ print(city[4:1:-1])
 print(city[-1:-4:-1])
 '''
 
-
+'''
 institute = "the kiran academy"
 
 
@@ -56,7 +56,7 @@ print(institute[-1:-8:-1])
 print(institute[8:3:-1])
 print(institute[-7::1])
 print(institute[2::-1])
-
+'''
 
 
 lan = "python programming language"
