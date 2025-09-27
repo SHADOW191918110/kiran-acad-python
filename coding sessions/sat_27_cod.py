772eda2745a174872ea240ae4be02f751e37bed2
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-27 05:33:25 +0000

    On branch main Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	new file:   coding sessions/sat_27_cod.py

---
+++ coding sessions/sat_27_cod.py
@@ -0,0 +1,19 @@
+#-----------------------------------------------#
+#  write a question input and sum o it 
+
+#------------------------------------------------#
+
+
+#--------------------------------------------------#
+# check i number is palindrome or not.....using  loops 
+
+sen = eval(input("write a palindrome number"))
+a =""
+for i in en :
+
+    a = i+a
+    print (a)
+
+    
+
+
