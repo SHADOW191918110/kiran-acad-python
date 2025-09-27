b7a83665eae14dd913c2b77a994a5290e8982fd5
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-27 05:59:42 +0000

    Author:    SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
    On branch main
    Your branch and 'origin/main' have diverged,
    and have 1 and 1 different commits each, respectively.
    Changes to be committed:
    	modified:   coding sessions/sat_27_cod.py

--- coding sessions/sat_27_cod.py
+++ coding sessions/sat_27_cod.py
@@ -6,14 +6,35 @@
 
 #--------------------------------------------------#
 # check i number is palindrome or not.....using  loops 
-
+'''
 sen = eval(input("write a palindrome number"))
-a =""
-for i in en :
-
+a ={}
+for i in sen :
     a = i+a
     print (a)
 
-    
+'''
+
+# ---------------------------------------- #
 
 
+# ----------------------------------------#
+
+# how many character are in the the name or sentence  
+
+
+
+
+
+# ---------------------------------------------- #
+
+# ---------------------------------------------- #
+# write a program i a number is prime or not 
+
+
+num = int(input("give a number "))
+
+if num % num== 0 & num % 2 == 0 :
+    print("number is prime")
+else:
+    print("number is not prime")
