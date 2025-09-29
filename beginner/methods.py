bc7629391cc5e1204164e073ab22dcfd70319cdc
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-29 04:45:01 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   beginner/methods.py
    	new file:   beginner/tempCodeRunnerFile.py

--- beginner/methods.py
+++ beginner/methods.py
@@ -1,9 +1,34 @@
-isalpha() :
-password= 'vaibhav'
-print(password.isalpha())
-password= 'vaibhavl 23 '
-print(password.isalpha())
-#True
-#False
-institute= 'the kiran academy'
-print(institute.isalpha()) #False
+# isalpha() :
+# password= 'vaibhav'
+# print(password.isalpha())
+# password= 'vaibhavl 23 '
+# print(password.isalpha())
+# #True
+# #False
+# institute= 'the kiran academy'
+# print(institute.isalpha()) #False
+
+
+# METHODS 
+
+#  methods takes parameter 
+
+
+# .index()
+#  .count()
+#  .remove()
+#  .startswith
+#  .endswith
+
+name = "sairaj rajendra khilari"
+
+print(name.index("j"))  # : 5
+
+print(name.count('a'))  # :5
+
+print(name.count('a',0,-8))  # : 4
+print(name.startswith('s',1,10))  
+print(name.endswith('i'))  
+
+
+
---
+++ beginner/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+name = "sairaj rajendra khilari"
