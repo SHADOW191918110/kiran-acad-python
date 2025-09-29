37423e82c83122cbfe7490f8ef3699e8bcf3c8df
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-09-29 03:57:11 +0000

    On branch main Your branch and 'origin/main' have diverged, and have 5 and 1 different commits each, respectively.
    Author:    SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
    Changes to be committed:
    	modified:   beginner/methods.py

---
+++ beginner/methods.py
@@ -0,0 +1,9 @@
+isalpha() :
+password= 'vaibhav'
+print(password.isalpha())
+password= 'vaibhavl 23 '
+print(password.isalpha())
+#True
+#False
+institute= 'the kiran academy'
+print(institute.isalpha()) #False
