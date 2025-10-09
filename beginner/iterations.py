52987728a99779acce19fd83ebc4c92264c3f53e
SAIRAJ KHILARI <157227948+SHADOW191918110@users.noreply.github.com>
2025-10-09 04:53:26 +0000

    d

--- beginner/iterations.py
+++ beginner/iterations.py
@@ -1,6 +1,75 @@
-write a program to print square of each no from an iterable
+# write a program to print square of each no from an iterable
 
-a = 10
 
-i in range (a)
-    print(i*i)
+
+students = ["sai","raj","kunal"]
+sb = []
+for i in students :
+    a =i.upper()
+    sb.append(a)
+
+
+
+# prog create a dict from to represent square of each number 
+
+number =[1,2,3,4,5,6,7,8,9,10]
+squ ={}
+for i in number:
+    a = i*i
+    squ[i] = a
+print(squ)
+
+
+
+# wap to cal sum of all numbers of list
+sum =0
+for i in number :
+    sum = sum + i
+print(sum)
+
+
+
+#wap to p
+products = {"mob" :20000,"lap":80000,"mou":150}
+
+price = 0
+for i in products.values():
+    price = price + i
+print( price )
+
+
+# wap to cal per✓✓
+marks = {'test': 70,'test1':80,'test2':65}
+per = 0
+aa = marks.__len__
+for i in marks:
+    
+
+    a 
+print()
+#
+
+oppo= {'a15':20000,'a16':25000,'a19':30000}
+total =0
+aaa =len(oppo)
+
+for i in oppo :
+    t =i
+
+print()
+
+#. a
+# 
+emp_sal = {'kunal':40000,'om':50000,'ajay':55000,'pranav':70000}
+
+for i in emp_sal.values :
+    
+
+
+
+
+
+
+
+
+
